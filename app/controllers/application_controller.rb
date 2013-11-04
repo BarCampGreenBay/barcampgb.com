fd67d11d91b996446731fb8d5eb28c0391baa9a8
Chris Schmitz <ccschmitz@gmail.com>
2013-11-03 20:52:22 -0600

    Add signed_in? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # what happens when a user isn't logged in - might want to do something besides redirect home
   def login!
-    redirect_to root_path unless current_user
+    redirect_to new_session_path unless current_user
   end
 
   private
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def signed_in?
+    !current_user.nil?
+  end
+  helper_method :signed_in?
 end
