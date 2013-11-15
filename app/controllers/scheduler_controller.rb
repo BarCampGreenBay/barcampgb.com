59ea52b4cf6966e3604ad02ef03ae26596583001
Andrew Hooker <geekoncoffee@gmail.com>
2013-11-14 21:38:21 -0600

    Add view for users who are signed up for both talks

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -1,8 +1,14 @@
 class SchedulerController < ApplicationController
-  def index
+  def people
     raise ActionController::RoutingError.new('Not Found') unless current_user.is_admin?
 
     @presentations = Presentation.includes(:users).load.reject{|p| p.users.empty?}.sort{|p| p.users.size}.reverse
     @users = User.includes(:presentations).reject{|u| u.presentations.empty?}.sort{|u| u.presentations.size}.reverse
+  end
+
+  def sessions
+    raise ActionController::RoutingError.new('Not Found') unless current_user.is_admin?
+
+    @presentations = Presentation.includes(:users).load.sort{|p| p.users.size}.reverse
   end
 end
